4a331362582c7f71fa159e0c117a862969fa88ad
Roger Espinoza <rj.espinoza44@gmail.com>
2023-04-04 09:36:25 -0500

    Initial Commit

---
+++ OtakuApp/OtakuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtakuAppApp.swift
+//  OtakuApp
+//
+//  Created by Roger Espinoza on 4/4/23.
+//
+
+import SwiftUI
+
+@main
+struct OtakuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
